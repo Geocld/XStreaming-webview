0c81cb9d306e8fc74ebb8ff176055cf9661abbd1
Geocld <lijiahao5372@gmail.com>
2024-06-23 21:48:22 +0800

    feat: gamepad maping

--- src/pages/map/Map.tsx
+++ src/pages/map/Map.tsx
@@ -1,26 +1,47 @@
-import { useState } from 'react'
-import { useGamepads } from 'react-gamepads';
+import { useState, useEffect } from 'react'
 import { Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@nextui-org/react";
-import './Map.css'
+import GamepadMapModal from '../../components/GamepadMapModal';
+import MapItem from '../../components/MapItem';
+import './Map.scss'
+
+const defaultMaping = {
+  'A': 0,
+  'B': 1,
+  'X': 2,
+  'Y': 3,
+  'DPadUp': 12,
+  'DPadDown': 13,
+  'DPadLeft': 14,
+  'DPadRight': 15,
+  'LeftShoulder': 4,
+  'RightShoulder': 5,
+  'LeftThumb': 10,
+  'RightThumb': 11,
+  'LeftTrigger': 6,
+  'RightTrigger': 7,
+  'Menu': 9,
+  'View': 8,
+  'Nexus': 16,
+}
 
 const buttonLabels = [
   "A",
   "B",
   "X",
   "Y",
-  "L1",
-  "R1",
-  "L2",
-  "R2",
-  "Back",
-  "Start",
-  "L3",
-  "R3",
-  "UP",
-  "DOWN",
-  "LEFT",
-  "RIGHT",
-  "XBOX",
+  "DPadUp",
+  "DPadDown",
+  "DPadLeft",
+  "DPadRight",
+  "LeftShoulder",
+  "RightShoulder",
+  "LeftTrigger",
+  "RightTrigger",
+  "LeftThumb",
+  "RightThumb",
+  "View",
+  "Menu",
+  "Nexus",
 ]
 
 const axesLabels = [
@@ -31,88 +52,71 @@ const axesLabels = [
 ]
 
 function Map() {
-  const [maping, setMaping] = useState({
-    'A': 0,
-    'B': 1,
-    'X': 2,
-    'Y': 3,
-    'DPadUp': 12,
-    'DPadDown': 13,
-    'DPadLeft': 14,
-    'DPadRight': 15,
-    'LeftShoulder': 4,
-    'RightShoulder': 5,
-    'LeftThumb': 10,
-    'RightThumb': 11,
-    'LeftTrigger': 6,
-    'RightTrigger': 7,
-    'Menu': 9,
-    'View': 8,
-    'Nexus': 16,
-  })
+  const [maping, setMaping] = useState(JSON.parse(JSON.stringify(defaultMaping)))
+  const [current, setCurrent] = useState('')
 
-  const [gamepads, setGamepads] = useState([])
+  useEffect(() => {
+    // document.body.style.height = 'auto'
+    document.body.style['overflow-y'] = 'auto'
+    document.body.style['position'] = 'inherit'
+  }, [])
+
   const [showModal, setShowModal] = useState(false)
 
-  useGamepads(_gamepads => {
-    setGamepads(Object.values(_gamepads))
-  })
-  if (!gamepads) return ''
-
-  if (gamepads[0]) {
-    gamepads[0].buttons.forEach((b, idx) => {
-      if (b.pressed) {
-        console.log('press:', idx)
-      }
+  const handleMapConfirm = (name, idx) => {
+    console.log(name, idx)
+    setShowModal(false)
+    setMaping({
+      ...maping,
+      [name]: idx
     })
+  }
+
+  const handleMapPress = (name) => {
+    setCurrent(name)
+    setShowModal(true)
+  }
+
+  const handleSave = () => {
+    console.log('maping:', maping)
+  }
+
+  const handleReset = () => {
+    setMaping(defaultMaping)
   }
 
   return (
     <div className='map-page'>
-      <Modal isOpen={showModal}>
-        <ModalContent>
-          <>
-            <ModalHeader className="flex flex-col gap-1">映射A按键</ModalHeader>
-            <ModalBody>
-              <p> 
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Nullam pulvinar risus non risus hendrerit venenatis.
-                Pellentesque sit amet hendrerit risus, sed porttitor quam.
-              </p>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="danger" variant="light" onPress={() => setShowModal(false)}>
-                Close
-              </Button>
-              <Button color="primary" onPress={() => setShowModal(false)}>
-                Action
-              </Button>
-            </ModalFooter>
-          </>
-        </ModalContent>
-      </Modal>
-      <Button color="primary" onPress={() => { setShowModal(true) }}>
-        A
-      </Button>
-      <div>
-      {gamepads.length && gamepads.map(gp => {
-        return (
-          <div key={gp}>
-            <div><span>ID:</span>{gp.id}</div>
-            {gp.buttons.map((button, index) => {
-              return (
-                <div key={index}><span>{buttonLabels[index]}:</span><span>{button.value}</span></div>
-              )
-            })}
-            {gp.axes.map((stick, index) => {
-              return (
-                <div key={index}><span>{axesLabels[index]}:</span><span>{stick}</span></div>
-              )
-            })}
-          </div>
+
+      {
+        showModal && (
+          <GamepadMapModal show={showModal} current={current} onConfirm={handleMapConfirm} onCancel={() => setShowModal(false)}/>
         )
-      })}
+      }
+      
+  
+      <div className='maps'>
+        {
+          buttonLabels.map(name => {
+            return (
+              <div className='maps-item' key={name}>
+                <MapItem name={name} value={maping[name]} onPress={handleMapPress}/>
+              </div>
+            )
+          })
+        }
+
+        <div className="operate-btns">
+          <Button color="primary" fullWidth onPress={handleSave}>
+            保存
+          </Button>
+          <Button color="default" fullWidth onPress={handleReset}>
+            恢复默认值
+          </Button>
+        </div>
+      
       </div>
+      
     </div>
   )
 }
